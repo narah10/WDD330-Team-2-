f4b143d439a48ed94dc6829d129fcd91d217d287
Lisa <lisacjheinhold@gmail.com>
2023-09-27 13:45:27 -0600

    fixed empty cart error

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,14 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const productList = document.querySelector(".product-list");
+  if (cartItems) {
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    productList.innerHTML = htmlItems.join("");
+  } else {
+    const emptyCartMessage = `<p>No items in cart.</p>`
+    productList.innerHTML = emptyCartMessage
+  }
 }
 
 function cartItemTemplate(item) {
