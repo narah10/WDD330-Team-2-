0e5222151a4c4d612be4a03d3f1165adc0e0744d
arden <ardenbyui.edu>
2023-10-10 10:19:39 -0500

    superscript final

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import ProductList from "./components/ProductList.svelte";
+import { itemAmountInCart } from "./utils.mjs";
+
 
 new ProductList({
     target: document.querySelector(".products"),
     props: { category: "tents" },
 });
+
+itemAmountInCart(); 
