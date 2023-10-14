8197282a058a52184433c15d840e73bc2fdab9f6
arden <ardenbyui.edu>
2023-10-14 15:50:48 -0600

    call-to-action

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,33 @@ new ProductList({
     target: document.querySelector(".products"),
     props: { category: "tents" },
 });
+
+function checkVisit(){
+if(!localStorage.getItem('lastvisit')){
+    localStorage.setItem('lastvisit', Date.now()); 
+    firstTimeVisit();
+} 
+else{
+   console.log('not first visit')
+}};
+checkVisit();
+
+
+function firstTimeVisit(){
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
+
+document.querySelector('.pButton').addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
+}
