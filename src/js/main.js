53e14a0b6d84141038fe558b1456bc9190ca8442
arden <ardenbyui.edu>
2023-10-17 14:56:24 -0600

    new call to action first visit

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,33 @@ new ProductList({
 });
 
 renderHeaderFooter();
+
+function checkVisit(){
+    if(!localStorage.getItem('lastvisit')){
+        localStorage.setItem('lastvisit', Date.now()); 
+        firstTimeVisit();
+    } 
+    else{
+       console.log('not first visit')
+    }};
+    checkVisit();
+    
+    
+    function firstTimeVisit(){
+    
+    window.addEventListener("load", function(){
+        setTimeout(
+            function open(event){
+                document.querySelector(".popup").style.display = "block";
+            },
+            1000
+        )
+    });
+    document.querySelector("#close").addEventListener("click", function(){
+        document.querySelector(".popup").style.display = "none";
+    });
+    
+    document.querySelector('.pButton').addEventListener("click", function(){
+        document.querySelector(".popup").style.display = "none";
+    });
+    }
