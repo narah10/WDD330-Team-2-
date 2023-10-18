ad90b366872e7fc2b8ad211ef8465021c45ef66c
Lisa <lisacjheinhold@gmail.com>
2023-10-18 10:00:10 -0600

    finished alert - lisa

---
+++ src/js/components/Alert.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { alerts } from '../../alert/alert.json';
+</script>
+
+<section class="alert-list">
+    {#each alerts as alert }
+        <div class="alert" style="background-color: { alert.background }; color: { alert.color };">
+            <p>{ alert.message }</p>
+        </div>
+    {/each}
+</section>
+
+<style>
+.alert-list {
+    text-align: center;
+    display: flex;
+    flex-flow: column nowrap;
+}
+.alert {
+    height: 5vh;
+    text-align: center;
+    width: 100%;
+}
+</style>
