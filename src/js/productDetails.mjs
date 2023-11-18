21a1bb4f5f397ffc4dc740e3cc307810b4d62c02
Mitzi Vite <vit20001@byui.edu>
2023-11-18 11:21:04 -0700

    colors filter

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -41,16 +41,17 @@ function getBreadcrumbText(category) {
 
 let product = {};
 
+// Main function for product details
 export default async function productDetails(productId, selector) {
   try {
     product = await findProductById(productId);
     if (product) {
-      // Create Breadcrumb
+      // Create breadcrumb
       const breadcrumbContainer = document.createElement('div');
-      breadcrumbContainer.id = 'breadcrumbContainer'; // You may customize the ID
+      breadcrumbContainer.id = 'breadcrumbContainer'; // Customize the ID if needed
       createBreadcrumb(breadcrumbContainer, product.Category);
 
-      // Once we have the product details, we can render out the HTML
+      // Once we have the product details, we can render the HTML
       const el = document.querySelector(selector);
       el.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
       el.insertBefore(breadcrumbContainer, el.firstChild);
@@ -60,6 +61,25 @@ export default async function productDetails(productId, selector) {
         addProductToCart();
         showCartMessage();
       });
+
+      // Add an event listener to change the main image when a color thumbnail is clicked
+      document.addEventListener('click', function(event) {
+        const colorThumbnail = event.target.closest('.color-thumbnail');
+        if (colorThumbnail) {
+          const selectedColorCode = colorThumbnail.dataset.color;
+          const colorImage = product.Colors.find(color => color.ColorCode === selectedColorCode).ColorPreviewImageSrc;
+
+          // Change the main image to the selected color
+          document.querySelector('.main-product-image').src = colorImage;
+
+          // Update the value of the hidden radio input (optional)
+          const colorRadio = document.querySelector(`input[name="color"][value="${selectedColorCode}"]`);
+          if (colorRadio) {
+            colorRadio.checked = true;
+          }
+        }
+      });
+
     } else {
       showError("Sorry, this Product was not found.", selector);
     }
@@ -68,26 +88,25 @@ export default async function productDetails(productId, selector) {
   }
 }
 
+// Function to add the product to the cart
 function addProductToCart() {
   const cart = getLocalStorage("so-cart")  
   const currentItem = cart.find(item => item.Id == product.Id)
-  // get the current cart contents
-    if(currentItem){   // Check to see if current item is already in the cart
-      currentItem.quantity++;
-      console.log("already in Cart") 
-      setLocalStorage("so-cart", cart);
-        // if yes then add 1 to quantity
-    }else{
-      product.quantity = 1; 
-      setLocalStorage("so-cart", product);   // else add a quantity property to the product and insert into the cart 
-    // and insert into the cart 
-      console.log("item-added");
-    }
 
-  // setLocalStorage("so-cart", product);
-  // console.log("item-added");
+  // Get the current cart contents
+  if(currentItem){   // Check if the current item is already in the cart
+    currentItem.quantity++;
+    console.log("Already in Cart") 
+    setLocalStorage("so-cart", cart);
+      // If yes, add 1 to the quantity
+  } else {
+    product.quantity = 1; 
+    setLocalStorage("so-cart", product);   // If not, add a quantity property to the product and insert into the cart 
+    console.log("Item added");
+  }
 }
 
+// Function to show the cart message
 function showCartMessage() {
   const cartMessage = document.getElementById('cartMessage');
   cartMessage.style.display = 'block';
@@ -99,23 +118,29 @@ function showCartMessage() {
 
 function productDetailsTemplate(product) {
   const discount = calculateDiscount(product);
-  return `<h3>${product.Brand.Name}</h3>
 
-  <h2 class="divider">${product.NameWithoutBrand}</h2>
+  // Create HTML for color thumbnails
+  const colorThumbnailsHTML = product.Colors.map(color => `
+    <img src="${color.ColorPreviewImageSrc}" alt="${color.ColorName}" class="color-thumbnail" data-color="${color.ColorCode}">
+  `).join('');
 
-  <img
-    class="divider"
-    src="${product.Images.PrimaryLarge}"
-    alt="${product.NameWithoutBrand}"
-  />
-
-  <del class="cart-card__disccount">$${product.SuggestedRetailPrice}</del>
-  <p class="product-card__price product_price">$${product.FinalPrice}</p>
-  <p class="discount_price">REDUCED PRICE You Save: ${discount.percentage.toFixed(2)}%</p>
-  <p class="product__color">${product.Colors[0].ColorName}</p>
-  <p class="product__description">${product.DescriptionHtmlSimple}</p>
-  <div class="product-detail__add">
-    <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    <div id="cartMessage" class="hidden">Item added to the cart</div>
-  </div>`;
+  return `
+    <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <div class="product-images">
+      <div class="main-product-image-container">
+        <img class="main-product-image" src="${product.Images.PrimaryLarge}" alt="${product.NameWithoutBrand}" />
+      </div>
+      <div class="color-thumbnails-container">
+        ${colorThumbnailsHTML}
+      </div>
+    </div>
+    <del class="cart-card__disccount">$${product.SuggestedRetailPrice}</del>
+    <p class="product-card__price product_price">$${product.FinalPrice}</p>
+    <p class="discount_price">REDUCED PRICE You Save: ${discount.percentage.toFixed(2)}%</p>
+    <p class="product__description">${product.DescriptionHtmlSimple}</p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+      <div id="cartMessage" class="hidden">Item added to the cart</div>
+    </div>`;
 }
