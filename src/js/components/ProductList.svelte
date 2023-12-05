756d01cf2a0e00c3e83217c9a48bfb98a4d0888e
Axa Uribe <uri19002@byui.edu>
2023-12-05 11:09:37 -0700

    Discount Flag

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,15 +1,13 @@
 <script>
   import ProductSummary from "./ProductSummary.svelte";
   import { getData } from "../productData.mjs";
+  import { calculateDiscount } from "../product.js";
 
   export let category;
 
   let promise = getData(category);
- 
+  
 </script>
-
-
-
 
 <h2>Top Products {category}</h2>
 {#await promise}
@@ -17,7 +15,24 @@
 {:then data}
   <ul class="product-list">
     {#each data as product}
-        <li class="product-card"><ProductSummary {product} /></li>
-         {/each}
+      {#if product}
+        <li class="product-card">
+          <ProductSummary {product} />
+          <p>Discount: {calculateDiscount(product).percentage.toFixed(2)}%</p>
+        </li>
+      {/if}
+    {/each}
   </ul>
 {/await}
+
+<style>
+  .product-card p{
+    position: relative;
+    background-color: rgb(194, 194, 23);
+    padding: 0.5rem;
+    border-radius: 15px;
+    text-align: center;
+    font-weight: bold;
+  }
+</style>
+
